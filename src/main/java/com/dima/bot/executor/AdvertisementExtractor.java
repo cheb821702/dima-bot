30da25a109fc80592b20ff6eb5352eaa1e47b983
cheb821702 <cheb821702@mail.ru>
2014-04-18 17:16:20 +0300

    Advertisement

--- src/main/java/com/dima/bot/executor/AdvertisementExtractor.java
+++ src/main/java/com/dima/bot/executor/AdvertisementExtractor.java
@@ -4,9 +4,16 @@ import com.dima.bot.util.HttpManager;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -33,7 +40,7 @@ public class AdvertisementExtractor {
             String title = doc.title();
             System.out.println("title : " + title);
 
-            // get all links
+
             Element body = doc.body();
             for(Element bodyChild : body.children()) {
                 if("form".equals(bodyChild.tagName()) && bodyChild.hasAttr("method") && "post".equals(bodyChild.attr("method"))) {
@@ -41,13 +48,72 @@ public class AdvertisementExtractor {
                         if("table".equals(formChild.tagName()))
                         for(Element tableChild : formChild.children()) {
                             if("tbody".equals(tableChild.tagName())) {
-                                int num = 0;
+                                int numTr = 0;
                                 for(Element tbodyChild : tableChild.children()) {
                                     if("tr".equals(tbodyChild.tagName())) {
-                                        if(0 < num && num < 21) {
-
+                                        if(0 < numTr && numTr < 21) {
+                                            int numTd = 0;
+                                            for (Element trChild : tbodyChild.children()) {
+                                                if("td".equals(trChild.tagName())) {
+                                                    if(numTd == 1) {
+                                                        for (TextNode tdtext : trChild.textNodes()) {
+                                                            if (tdtext.text().trim().matches("\\d{4}-\\d{2}-\\d{2}\\s+\\d{2}:\\d{2}:\\d{2}")) {
+                                                                try {
+                                                                    Date date = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss", Locale.ENGLISH).parse(tdtext.text().trim());
+                                                                    System.out.println(date.toString());
+                                                                } catch (ParseException e) {
+                                                                    e.printStackTrace();
+                                                                }
+                                                            }
+                                                        }
+                                                        for (Element tdChild : trChild.children()) {
+                                                            if("small".equals(tdChild.tagName())) {
+                                                                System.out.println(tdChild.text().trim());
+                                                            }
+                                                        }
+                                                    } else if(numTd == 2) {
+                                                        if(trChild.textNodes().size() != 0) {
+                                                            System.out.println(trChild.textNodes().get(0).text().trim());
+                                                            if(trChild.textNodes().size() != 1) {
+                                                                System.out.println(trChild.textNodes().get(1).text().trim());
+                                                            }
+                                                        }
+                                                        for (Element tdChild : trChild.children()) {
+                                                            if("small".equals(tdChild.tagName())) {
+                                                                for (Element smallChild : tdChild.children()) {
+                                                                    if("font".equals(smallChild.tagName())) {
+                                                                        System.out.println(smallChild.text().trim());
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    } else if(numTd == 3) {
+                                                        String html = trChild.html();
+                                                        html = html.substring(html.indexOf("</i>")+4).trim();
+                                                        for(String htmlEl : html.split("<br />")) {
+                                                            if(!htmlEl.isEmpty()) {
+                                                                System.out.println(htmlEl.trim());
+                                                            }
+                                                        }
+                                                    } else if(numTd == 6) {
+                                                        for (Element tdChild : trChild.children()) {
+                                                            if("a".equals(tdChild.tagName())) {
+                                                                if("обработан".equals(tdChild.text().trim())) {
+                                                                    System.out.println("Зеленый");
+                                                                } else {
+                                                                    System.out.println("Красный");
+                                                                }
+                                                                if(tdChild.hasAttr("href")) {
+                                                                    System.out.println(tdChild.attr("href").trim());
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                    numTd++;
+                                                }
+                                            }
                                         }
-                                       num++;
+                                       numTr++;
                                     }
                                 }
                             }
--- src/main/java/com/dima/bot/executor/model/Advertisement.java
+++ src/main/java/com/dima/bot/executor/model/Advertisement.java
@@ -1,5 +1,9 @@
 package com.dima.bot.executor.model;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created with IntelliJ IDEA.
  * User: ShemiareiD
@@ -8,6 +12,16 @@ package com.dima.bot.executor.model;
  * To change this template use File | Settings | File Templates.
  */
 public class Advertisement {
+
+    private Date date;
+    private long number;
+    private String auto;
+    private int autoYear = -1;
+    private String autoCode;
+    private Map<String,String> details = new HashMap<String,String>();
+    private String openURL;
+    private boolean performed = false;
+
 
 
 
